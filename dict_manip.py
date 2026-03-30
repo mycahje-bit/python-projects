7daead8f1cf4cb79539d8534cd18bf8769c8a293
mycah <mycah.espolong@gmail.com>
2026-03-30 13:17:55 +0800

    notes 2

---
+++ dict_manip.py
@@ -0,0 +1,10 @@
+cart = [
+    {1: {"name" : "bread", "price" : 35, "qty": 2},
+    2: {"name" : "milk", "price" : 50, "qty": 1},
+    3: {"name" : "eggs", "price" : 8, "qty": 12}}
+]
+
+print(cart[0][1]["name"]) # bread
+print(cart[0][1]["price"]) # 35
+print(cart[0][1]["qty"]) # 2
+        
